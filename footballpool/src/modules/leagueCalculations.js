e1860ca2e0f9093d574c5da315421b024a722c79
David Kuehn <kuehnd96@gmail.com>
2020-07-10 17:03:21 -0500

    Creation of league calculations logic part 1

---
+++ footballpool/src/modules/leagueCalculations.js
@@ -0,0 +1,63 @@
+
+export default function leagueCalculations() {
+    
+    function calculateLeagueStats(leagueUserPicks) {
+
+        let leagueUserTotals = []
+        
+        // Foreach user in the league
+        leagueUserPicks.foreach((userPicks => {
+        
+            let leagueUserStatus = {
+                
+                totalPoints: 0,
+                weekResults: [],
+                userName: userPicks.userName
+            }
+            
+            // Foreach of the user's picks
+            userPicks.picks.foreach((pick => {
+
+                let points = 0
+                
+                // Does associated matchup have a result?
+                if (pick.matchupid.fields.didHomeTeamWin !== undefined) {
+                    
+                    // Did the current user guess right?
+                    if (pick.matchupid.fields.didHomeTeamWin === pick.ishomepick) {
+
+                        points = pick.points
+                    }
+                }
+
+                leagueUserStatus.weekResults.push({
+
+                    week: pick.matchupid.fields.week,
+                    points: points
+                })
+                
+                leagueUserStatus.totalPoints += points
+            }))
+
+            leagueUserTotals.push(leagueUserStatus)
+            
+        }))
+
+        return leagueUserTotals;
+    }
+
+    function sortLeagueUserTotalsByTotalPointsDescending(a, b) {
+        const totalA = a.totalPoints
+        const totalB = b.totalPoints
+
+        let comparison = 0
+
+        if (totalA > totalB) {
+            comparison = -1
+        } else if (totalA < totalB) {
+            comparison = 1
+        }
+        
+        return comparison
+    }
+}
